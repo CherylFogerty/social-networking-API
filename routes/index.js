079b937ebec1d3c26875b2ab2efd93fb434bde26
Cheryl Fogerty <cfogerty@aol.com>
2021-04-25 17:22:16 -0500

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 //add prefix of `/api` to all of the api routes imported from the `api` directory
-router.use('./api', apiRoutes);
+router.use('/api', apiRoutes);
 
 // router.use((req, res) => {
 //     return res.send('<h1>Wrong! You messed up. 404 Error</h1>');
