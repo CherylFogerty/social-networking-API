2ba8fdd097a3915c7cc85a633eabf68b596a180b
Cheryl Fogerty <cfogerty@aol.com>
2021-04-24 23:42:47 -0500

    update files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+// set up Mongoose to connect when we start the app.
+// MongoDB will find and connect to the database if it exists or create the database if it doesn't.
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// log mongo queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
